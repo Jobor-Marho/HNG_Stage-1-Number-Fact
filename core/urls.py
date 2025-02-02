a62148a0e56b9bfe129cb6e45fb2369445e30d73
Marho <joborfrederick0214@gmail.com>
2025-02-03 00:49:24 +0100

    added static

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 """
 Url mapping settings for project
 """
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path("api/", include("numbapp.urls"))
 ]
+
+urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
